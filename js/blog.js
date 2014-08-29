3cc129f3bebf9cc34dfa46ae6d63ad55d384e717
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2014-08-29 17:12:30 +0900

    ブログ用スクリプト追加

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+!function(){
+  console.log("samuiine");
+  frozenJsDomain = "http://nakayama-yuhei.github.io/3611/";
+  var s=document.createElement("script");
+  s.src=frozenJsDomain+"js/bookmarklet_main.js";
+  document.head.appendChild(s);
+}();
